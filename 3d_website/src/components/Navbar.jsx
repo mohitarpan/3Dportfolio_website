f4402083c0f4308d385a0a8df09753ecad6c6351
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-01-17 19:27:39 +0000

    few new package and navbar ready

---
+++ 3d_website/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <header className= "header"> 
+    <NavLink to="/">
+
+    </NavLink>
+    </header>
+  )
+}
+
+export default Navbar
