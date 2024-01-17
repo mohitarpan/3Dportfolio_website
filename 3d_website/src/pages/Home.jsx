0b6ab1525a2be9be52df5629c9eff0f85425f55e
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-01-17 21:34:16 +0000

    loader is used for load a three model

--- 3d_website/src/pages/Home.jsx
+++ 3d_website/src/pages/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import {Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
+{/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+  POPUP
+</div> */}
+{/* <div className='absolute top-0 left-0 right-0 bottom-0 '></div> */}
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full h-screen relative'>
+      <Canvas 
+      className='w-full h-screen bg-transparent' 
+      camera={{near:0.1, far:1000}}
+      >
+        <Suspense fallback={<Loader/>}>
+          <directionalLight/>
+          <ambientLight/> 
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+        </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
