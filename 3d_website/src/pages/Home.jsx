dbb568911050a48c65b77d61a0d8011c9c5a5b4e
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-01-18 19:32:32 +0000

    1

--- 3d_website/src/pages/Home.jsx
+++ 3d_website/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import {Suspense} from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import Island from '../Models/Island';
+import Sky from '../Models/Sky';
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
   POPUP
@@ -9,6 +10,19 @@ import Island from '../Models/Island';
 {/* <div className='absolute top-0 left-0 right-0 bottom-0 '></div> */}
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+   let screenScale = null 
+   let screenPostion = [0,-6.5,-43];
+   let rotation = [0.1,4.7,0];
+
+   if(window.innerWidth < 768) {
+    screenScale = [0.9,0.9,0.9];
+   } else {
+    screenScale = [1,1,1];
+   }
+   return [screenScale,screenPostion,rotation]
+  }
+  const [islandScale,islandPostion,islandRotation] = adjustIslandForScreenSize();
   return (
     <section className='w-full h-screen relative'>
       <Canvas 
@@ -16,12 +30,17 @@ const Home = () => {
       camera={{near:0.1, far:1000}}
       >
         <Suspense fallback={<Loader/>}>
-          <directionalLight/>
-          <ambientLight/> 
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
-          <Island/>
+          <directionalLight position={[1,1,1]} intensity={2}/>
+          <ambientLight intensity={0.5}/> 
+          {/* <pointLight  />
+          <spotLight /> */}
+          <hemisphereLight skycolor="#b1eff" groundColor={"#000000"} intensity={1}/>
+          <Sky />
+          <Island 
+          position={islandPostion}
+          scale ={islandScale}
+          rotation ={islandRotation}
+          />
         </Suspense>
       </Canvas>
     </section>
